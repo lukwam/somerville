cbe189f02a1b274c9ef4292cfc291a22e4ec6a76
Lukas Karlsson <admin@lukwam.dev>
2021-09-25 22:54:09 +0000

    Added a firestore collection to store events with unique ids.

--- functions/update_meeting_calendar/main.py
+++ functions/update_meeting_calendar/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Cloud Function to update the Somerville Meeting Calendar in Google Calendar."""
+import random
 from datetime import datetime
 from datetime import timedelta
 from time import mktime
@@ -7,9 +8,10 @@ from urllib.parse import urlparse
 
 import dateparser
 import feedparser
+from google.cloud import firestore
 from googleapiclient.discovery import build
 from pytz import timezone
-
+from pytz import utc
 
 CALENDAR_ID = "rfkur2e12ehe3kcd712b28kgpo@group.calendar.google.com"
 RSS_FEED = "http://somervillecityma.iqm2.com/Services/RSS.aspx?Feed=Calendar"
@@ -38,6 +40,7 @@ def get_google_calendar_events():
     service = build('calendar', 'v3', cache_discovery=False)
     params = {
         "calendarId": CALENDAR_ID,
+        "timeZone": "America/New_York",
     }
     events = service.events()
     request = events.list(**params)
@@ -62,6 +65,7 @@ def get_google_calendar_events_dict():
 def get_rss_entries():
     """Return a list of entries from the Somerville Meeting Calendar RSS feed."""
     feed = feedparser.parse(RSS_FEED)
+    tz = timezone("America/New_York")
     items = []
     for entry in sorted(feed["entries"], key=lambda x: x["id"]):
         entry_id = entry["id"]
@@ -78,10 +82,10 @@ def get_rss_entries():
         meeting_name, meeting_type, date_string = title.split(" - ")
 
         # parse date string
-        meeting_date = dateparser.parse(date_string)
+        meeting_date = tz.localize(dateparser.parse(date_string)).astimezone(utc)
 
         # parse published time struct
-        published = datetime.fromtimestamp(mktime(published_time))
+        published = tz.localize(datetime.fromtimestamp(mktime(published_time))).astimezone(utc)
 
         item = {
             "id": meeting_id,
@@ -106,7 +110,7 @@ def get_rss_meetings(entries):
         mid = entry["id"]
         if mid not in meetings:
             meetings[mid] = {
-                "id": mid,
+                "meeting_id": mid,
                 "date": entry["date"],
                 "link": entry["link"],
                 "name": entry["name"],
@@ -118,21 +122,19 @@ def get_rss_meetings(entries):
 
 def prepare_rss_events(meetings):
     """Return a dict of RSS meetings in Google Calendar event format."""
-    tz = timezone('America/New_York')
     events = {}
-    for mid, meeting in meetings.items():
-        event_id = f"somerville{mid}"
-        start = tz.localize(meeting["date"])
+    for meeting in meetings.values():
+        event_id = meeting["id"]
+        start = meeting["date"].astimezone(timezone("America/New_York"))
         end = start + timedelta(hours=1)
-        description = (
-            f"Meeting Link: {meeting['link']}"
-        )
+        description = f"Meeting Link: {meeting['link']}"
         event = {
             "id": event_id,
             "summary": meeting["name"],
             "description": description,
-            "start": {"dateTime": start.isoformat()},
-            "end": {"dateTime": end.isoformat()},
+            "start": {"dateTime": start.isoformat(), "timeZone": "America/New_York"},
+            "end": {"dateTime": end.isoformat(), "timeZone": "America/New_York"},
+
         }
         events[event_id] = event
     return events
@@ -179,6 +181,68 @@ def update_events(old_events, new_events):
     print(f"Updated: {len(updated)}")
 
 
+def update_firestore_meetings(meetings):
+    """Update meetings in firestore and generate unique IDs."""
+    client = firestore.Client()
+    collection = "meetings"
+    ref = client.collection(collection)
+
+    current = {}
+    for doc in ref.stream():
+        meeting = doc.to_dict()
+        meeting_id = meeting["meeting_id"]
+        meeting["event_id"] = doc.id
+        current[meeting_id] = meeting
+    print(f"Current Firestore meetings: {len(current)}")
+
+    add = []
+    for meeting_id in meetings:
+        if meeting_id not in current:
+            add.append(meetings[meeting_id])
+    print(f"Add to Firestore: {len(add)}")
+
+    delete = []
+    for meeting_id in current:
+        if meeting_id not in meetings:
+            delete.append(meetings[meeting_id])
+    print(f"Delete from Firestore: {len(delete)}")
+
+    # add new meetings to firestore and "current"
+    for meeting in add:
+        meeting_id = meeting["meeting_id"]
+        doc_id = ''.join(random.choices("abcdefghijklmnopqrstuv0123456789", k=10))
+        meeting["date"] = meeting["date"].replace(tzinfo=timezone("UTC"))
+        meeting["event_id"] = doc_id
+        ref.document(doc_id).set(meeting)
+        current[meeting_id] = meeting
+
+    # delete old meetings from firestore and "current"
+    for meeting in delete:
+        doc_id = meeting["event_id"]
+        meeting_id = meeting["meeting_id"]
+        ref.document(doc_id).delete()
+        del current[meeting_id]
+
+    # update meetings in firestore
+    for meeting_id in meetings:
+        meeting = meetings[meeting_id]
+        if meeting_id not in current:
+            continue
+        doc_id = current[meeting_id]["event_id"]
+        meeting["event_id"] = doc_id
+        meeting["date"] = meeting["date"].replace(tzinfo=timezone("UTC"))
+        ref.document(doc_id).set(meeting)
+
+    firestore_meetings = {}
+    for doc in ref.stream():
+        doc_id = doc.id
+        meeting = doc.to_dict()
+        meeting_id = meeting["meeting_id"]
+        meeting["id"] = doc_id
+        firestore_meetings[meeting_id] = meeting
+    return firestore_meetings
+
+
 def update_meeting_calendar(request):
     """Get events from Somerville Meeting Calendar."""
     print(f"Getting RSS entries from {RSS_FEED}...")
@@ -186,8 +250,12 @@ def update_meeting_calendar(request):
     print(f"RSS Entries: {len(entries)}")
 
     print("Getting meetings from RSS entries...")
-    meetings = get_rss_meetings(entries)
-    print(f"Meetings: {len(meetings)}")
+    rss_meetings = get_rss_meetings(entries)
+    print(f"RSS Meetings: {len(rss_meetings)}")
+
+    print("Update meetings in Firestore...")
+    meetings = update_firestore_meetings(rss_meetings)
+    print(f"Firestore Meetings: {len(meetings)}")
 
     print("Preparing events for Google Calendar...")
     rss_events = prepare_rss_events(meetings)
